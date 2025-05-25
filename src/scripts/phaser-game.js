ae1d43f10ba16a15e78debd20feaebe219301519
Arthur L Molina <arthur.molina.dev@gmail.com>
2025-05-25 00:05:45 -0300

    update: iniciando configuração do fundo da tela do jogo
    Imagem do fundo do jogo adicionada

--- src/scripts/phaser-game.js
+++ src/scripts/phaser-game.js
@@ -20,11 +20,13 @@ var config = {
 var game = new Phaser.Game(config) //instância o jogo com as configurações pré-definidas
 
 function preload() {
-
+    this.load.image('fundo', 'src/assets/fundo.png');
+    this.load.image('chao', 'chao/chao.png');
 }
 
 function create(){
-
+    this.background = this.add.image(0,300, 'fundo')//posicionando a imagem na posição x=0 y=300
+    this.background.setOrigin(0, 0.5); //para fazer a imagem começar do inicio no ponto definida na linha acima
 }
 
 function update(){

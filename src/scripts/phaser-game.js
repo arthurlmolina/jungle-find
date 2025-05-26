1beee12df00228ffaaa5509a4db1481da8ce66a5
Arthur L Molina <arthur.molina.dev@gmail.com>
2025-05-25 23:26:34 -0300

    update: adiciona fundo e o chão do jogo

--- src/scripts/phaser-game.js
+++ src/scripts/phaser-game.js
@@ -21,12 +21,26 @@ var game = new Phaser.Game(config) //instância o jogo com as configurações pr
 
 function preload() {
     this.load.image('fundo', 'src/assets/fundo.png');
-    this.load.image('chao', 'chao/chao.png');
+    this.load.image('chao', 'src/assets/chao.png');
 }
 
 function create(){
     this.background = this.add.image(0,300, 'fundo')//posicionando a imagem na posição x=0 y=300
     this.background.setOrigin(0, 0.5); //para fazer a imagem começar do inicio no ponto definida na linha acima
+
+    plataformas = this.physics.add.staticGroup(); //criando um novo grupo de fisica estática, objetos que não serão afetados pela física, exemplo: chão e plataformas
+
+  
+    plataformas.create(0,554, 'chao').setOrigin(0.1,0.5);
+
+    //configurando o tamanho do mundo do jogo 
+    // (onde começa eixo x, onde começa eixo y, largura do mundo, altura do mundo)      
+    this.physics.world.setBounds(0, 0, 4063, 600);
+
+    //configurando a câmera que seguirá o personagem 
+    this.cameras.main.setBounds(0, 0, 4063, 600);
+
+
 }
 
 function update(){
